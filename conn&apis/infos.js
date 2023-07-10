f667d0ce1fcc2667863b0eac46f132a61c5ca7db
Jehoshaphat Adediran <73774374+segunade-coder@users.noreply.github.com>
2023-07-10 10:19:30 +0100

    Create infos.js

---
+++ conn&apis/infos.js
@@ -0,0 +1,10 @@
+const database_name = "ushmoney_segun";
+const host = "31.22.4.18"
+const user =  "ushmoney_jeo_segun";
+const password =  "ushTeam123!";
+module.exports = {
+  database_name,
+  host,
+  user,
+  password
+}
